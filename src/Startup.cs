372c5225a6cecc72425a624fa3017906110f976d
ashok1282 <35157309+ashok1282@users.noreply.github.com>
2021-03-03 15:58:26 +0530

    Update Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -23,6 +23,7 @@ namespace src
 
         public IConfiguration Configuration { get; }
 
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
