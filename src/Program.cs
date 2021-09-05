6b31dadf75a6705c42bb32843503f727408582d2
Gaurav Garg <gaurav20march@gmail.com>
2021-09-05 19:06:16 +0530

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+ 
 namespace src
 {
     public class Program
